25067565885cd0127e38e0c1937e69d88dbe06bf
Hitham <hitham.ly@gmail.com>
2019-11-04 20:50:24 -0500

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -10,8 +10,8 @@ python hello.py
 
 # c
 echo "excute C"
-gcc hello.c -o hello
-./hello
+gcc hello.c -o helloc
+./helloc
 
 # c++
 echo "excute Cpp"
@@ -26,3 +26,9 @@ matlab -nodisplay -nosplash -nodesktop -r "run('hello.m');exit;" | tail -n +11
 echo "excute Java"
 javac hello.java
 java hello
+
+# Fortran
+echo "excute Fortran"
+gfortran -ffree-form hello.f
+mv a.out hellof
+./hellof 
